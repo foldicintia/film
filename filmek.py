b7336d84af6bf498879959b437e1e85b3627bc30
foldicintia <foldicintia2002@gmail.com>
2024-01-17 17:43:26 +0100

    próbálkozások

--- filmek.py
+++ filmek.py
@@ -7,8 +7,8 @@ Hány darab legalább 110 perces film van?
 
 Kérd be egy színész nevét, és ajánlj egy pár filmet a készletből, ha tudsz (film címét íratjuk ki, ha van ilyen)! Ha nincs ilyen nevű színész, akkor azt is tudasd!
 
-A 4-es feladat eredményét írasd ki fájlba is! 
-"""
+A 4-es feladat eredményét írasd ki fájlba is! """ #????
+
 
 from Film import Film
 
@@ -31,16 +31,46 @@ def beolvas():
         filmek_lista.append(film)
     return filmek_lista
 
-"""def legrovidebb(filmek_lista):
+def sorok(filmek_lista):
+    print(f"A filmek szama: {len(filmek_lista)}")
+
+
+def legrovidebb(filmek_lista):
     legrovidebb_index=0
     for i in range(0,len(filmek_lista),1):
         if filmek_lista[legrovidebb_index].perc >= filmek_lista[i].perc:
             legrovidebb_index=i
     return legrovidebb_index
-"""
+
 def szaztiz(filmek_lista):
     szamlalo= 0
     for i in range(0,len(filmek_lista),1):
-        if filmek_lista[i].perc > 110:
+        if filmek_lista[i].perc >= 110:
             szamlalo += 1
     return szamlalo
+
+def szinesz_bekeres(filmek_lista):
+    szinesz: str = str(input("Adja meg egy színész nevét!: "))
+    for i in range(0,len(filmek_lista),1):
+        if filmek_lista[i].foszereplo == szinesz:
+            valasz= print(f"{szinesz} a következő filmekben szerepel: {filmek_lista[i].cim}")
+        elif filmek_lista[i].foszereplo != szinesz:
+            print(f"Nem található olyan film a listában amiben {szinesz} szerepel.")
+    return valasz  
+
+
+def szinesz_bekeres(filmek_lista):
+    szinesz: str = input("Adja meg egy színész nevét!: ")
+    talalatok = []
+    for i in range(0,len(filmek_lista),1):
+        if filmek_lista[i].foszereplo == szinesz:
+            talalatok.append(filmek_lista[i].cim)
+            print(f"{szinesz} a következő filmekben szerepel: {talalatok}")
+        else:
+            print(f"{szinesz} egyik listán szereplő filmben sem szerepel")
+
+    return talalatok
+
+
+
+
--- main.py
+++ main.py
@@ -2,8 +2,13 @@ import filmek
 
 filmek_lista=filmek.beolvas()
 
-legrovidebb_index=filmek.legrovidebb(filmek_lista)
-print(f"A legrövidebb film: {legrovidebb_index}")
 
-szamlalo=filmek.szaztiz(filmek_lista)
-print(f"A 110 percnél rövidebb filmek száma: {szamlalo}")
+
+legrovidebb_index=filmek.legrovidebb(filmek_lista)
+print(f"A legrövidebb film: {filmek_lista[legrovidebb_index].cim}")
+
+# szamlalo=filmek.szaztiz(filmek_lista)
+# print(f"A 110 percnél rövidebb filmek száma: {szamlalo}")  --->  nem tetszik neki a >= mert azt állítja hogy az egyik string a masik int és igy nem tudja elvegezni a dolgat :(
+
+talalatok=filmek.szinesz_bekeres(filmek_lista)
+# else ágon nem csak egyszer írja ki a kiírnivalóját :(
